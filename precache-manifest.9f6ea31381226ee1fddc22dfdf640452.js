61904470d598261e611ab26f702dd0e2024d545d
Felipe Silva <felipe.florianopolis@gmail.com>
2020-05-20 02:41:52 -0300

    Updates

---
+++ precache-manifest.9f6ea31381226ee1fddc22dfdf640452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ba1864f1bfae5b414f98d92d8e64a5",
+    "url": "/covid-project/index.html"
+  },
+  {
+    "revision": "f40029d4997c1ea5667a",
+    "url": "/covid-project/static/css/main.07bef4a3.chunk.css"
+  },
+  {
+    "revision": "9461cedf1a87e9900f96",
+    "url": "/covid-project/static/js/2.0d365343.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-project/static/js/2.0d365343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40029d4997c1ea5667a",
+    "url": "/covid-project/static/js/main.c6839720.chunk.js"
+  },
+  {
+    "revision": "18531d08b3d9cc65f9b4",
+    "url": "/covid-project/static/js/runtime-main.ed5cf38e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-project/static/media/logo-covid.d7265326.png"
+  }
+]);
